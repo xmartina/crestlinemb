f567ff958ba7268742690b4fb4c9589d986049d2
unknown <martinsprince126@gmail.com>
2023-09-11 10:24:10 -0400

    updated

--- admin/create_edit_stock.php
+++ admin/create_edit_stock.php
@@ -2,4 +2,56 @@
 require_once ("./layout/header.php");
 ?>
 
+<?php
+if (isset($_POST['submit'])) {
+    $stock_title = $_POST['stock_title'];
+    $stock_amount = $_POST['stock_amount'];
+    $stock_interest = $_POST['stock_interest'];
+    $stock_duration = $_POST['stock_duration'];
+    $stock_status = $_POST['stock_status'];
+
+// SQL query to insert data into the database
+    $sql = "INSERT INTO stock_investment (stock_title, stock_amount, stock_interest, stock_duration, stock_status)
+            VALUES ('$stock_title', '$stock_amount', '$stock_interest', '$stock_duration', '$stock_status')";
+
+    if(true){
+        toast_alert('success','Plan added successfully','Approved');
+
+
+
+    }else{
+        toast_alert('error','Sorry something went wrong');
+
+
+    }
+
+    header('Location:'.$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
+    die;
+
+
+}
+?>
+
+<h2>Insert Stock Information</h2>
+<form method="POST">
+    <label for="stock_title">Stock Title:</label>
+    <input type="text" name="stock_title" required><br><br>
+
+    <label for="stock_amount">Stock Amount:</label>
+    <input type="text" name="stock_amount" required><br><br>
+
+    <label for="stock_interest">Stock Interest:</label>
+    <input type="text" name="stock_interest" required><br><br>
+
+    <label for="stock_duration">Stock Duration:</label>
+    <input type="text" name="stock_duration" required><br><br>
+
+    <label for="stock_status">Stock Status:</label>
+    <select name="stock_status" required>
+        <option value="Active">Active</option>
+        <option value="Inactive">Inactive</option>
+    </select><br><br>
+
+    <input type="submit" name="submit" value="Insert">
+</form>
 
