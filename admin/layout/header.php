a6dd9680fa044d16c9471cb3d0c0b4b6a8bf378f
unknown <martinsprince126@gmail.com>
2023-09-12 20:35:52 -0400

    updated

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -241,7 +241,12 @@ $adminName = $row['firstname']." ".$row['lastname'];
                     <a href="#stock_investment" data-active="false" class="menu-toggle">
                         <div class="base-menu">
                             <div class="base-icons">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart"><!-- https://feathericons.dev/?search=pie-chart&iconset=feather -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                        <path d="M21.21 15.89A10 10 0 1 1 8 2.83" />
+                                        <path d="M22 12A10 10 0 0 0 12 2v10z" />
+                                    </svg>
+                                </svg>
                             </div>
                             <span>Stock Investment</span>
                         </div>
