23825fe52c5cc3bc0d101f48dde9a1ad72194131
unknown <martinsprince126@gmail.com>
2023-09-12 20:33:36 -0400

    updated

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -237,6 +237,17 @@ $adminName = $row['firstname']." ".$row['lastname'];
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
                 </li>
 
+                <li class="menu">
+                    <a href="#stock_investment" data-active="false" class="menu-toggle">
+                        <div class="base-menu">
+                            <div class="base-icons">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                            </div>
+                            <span>Stock Investment</span>
+                        </div>
+                    </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                </li>
 
 
             </ul>
