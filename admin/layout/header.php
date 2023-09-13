0eb901581758c2a6c4c50ac8eb768592c7798edd
unknown <martinsprince126@gmail.com>
2023-09-12 22:22:38 -0400

    updated

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -278,13 +278,6 @@ $adminName = $row['firstname']." ".$row['lastname'];
                     <li>
                         <a href="./list_stock.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> List All Stocks </a>
                     </li>
-
-                    <li>
-                        <a href="./funduser.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Credit/Deibt User </a>
-                    </li>
-                    <li>
-                        <a href="./transfer.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Wire Transfer User </a>
-                    </li>
                 </ul>
             </div>
 
