2ca098c87a585812fc7e9c54bc98c7a9f97c9716
unknown <martinsprince126@gmail.com>
2023-09-12 22:51:06 -0400

    updated

--- admin/edit_stock.php
+++ admin/edit_stock.php
@@ -1,3 +1,15 @@
+<?php
+include_once("./layout/header.php");
+//require_once("./include/adminloginFunction.php");
+//include_once("../include/config.php");
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM stock_investment WHERE id =:id";
+$data = $conn->prepare($sql);
+$data->execute(['id'=>$id]);
+
+$row = $data->fetch(PDO::FETCH_ASSOC);
+?>
 
 
 <!--  BEGIN CONTENT AREA  -->
@@ -21,13 +33,13 @@
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
                                                 <label for="">Stock Title:</label>
-                                                <input type="text"  name="stock_title" class="form-control" id="" placeholder="Stock Title:" value="<?= $row['stock_title'] ?>">
+                                                <input type="text"  name="stock_title" class="form-control" id="" placeholder="Stock Title:" value="<?php $row['stock_title'] ?>">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
                                                 <label for="">Stock Amount: <span class="text-white bg-info p-1 rounded shadow-sm">USD</span>  </label>
-                                                <input type="number"  name="stock_amount" class="form-control" id="" placeholder="Stock Amount:" value="<?= $row['stock_amount'] ?>">
+                                                <input type="number"  name="stock_amount" class="form-control" id="" placeholder="Stock Amount:" value="<?php $row['stock_amount'] ?>">
                                             </div>
                                         </div>
                                     </div>
@@ -35,13 +47,13 @@
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
                                                 <label for="">Stock Interest: <span class="text-white bg-info p-1 rounded shadow-sm">%</span>  </label>
-                                                <input type="number"  name="stock_interest" class="form-control" id="" placeholder="Stock Interest:" value="<?= $row['stock_interest'] ?>" >
+                                                <input type="number"  name="stock_interest" class="form-control" id="" placeholder="Stock Interest:" value="<?php $row['stock_interest'] ?>" >
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
                                                 <label for="">Stock Duration: <span class="text-white bg-info p-1 rounded shadow-sm">Days</span> </label>
-                                                <input type="number"  name="stock_duration" class="form-control" id="" placeholder="Stock Duration:" value="<?= $row['stock_duration'] ?>">
+                                                <input type="number"  name="stock_duration" class="form-control" id="" placeholder="Stock Duration:" value="<?php $row['stock_duration'] ?>">
                                             </div>
                                         </div>
                                     </div>
@@ -49,7 +61,7 @@
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
                                                 <?php
-                                                    if($row['stock_status'] = 1){ $stock_status = "Active" ;}elseif ($row['stock_status'] = 2){ $stock_status = "Inactive";}
+                                                    if($row['stock_status'] == 1){ $stock_status = "Active" ;}elseif ($row['stock_status'] == 2){ $stock_status = "Inactive";}
                                                 ?>
                                                 <label for="">Stock Status:</label>
                                                 <select name="stock_status" id="" class="form-control basic" value="<?php echo $stock_status; ?>">
