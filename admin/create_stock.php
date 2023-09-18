4e39a5fca677740f346da99a71d5bcde29fca1b7
unknown <martinsprince126@gmail.com>
2023-09-17 21:34:39 -0400

    updated

--- admin/create_stock.php
+++ admin/create_stock.php
@@ -8,19 +8,21 @@ require_once ("./layout/header.php");
 
 if (isset($_POST['create_stock'])) {
     $stock_title = $_POST['stock_title'];
-    $stock_amount = $_POST['stock_amount'];
+    $stock_amount_min = $_POST['stock_amount_min'];
+    $stock_amount_max = $_POST['stock_amount_max'];
     $stock_interest = $_POST['stock_interest'];
     $stock_duration = $_POST['stock_duration'];
     $stock_status = $_POST['stock_status'];
     $stock_id = str_pad(rand(0, 9999999999), 10, '0', STR_PAD_LEFT);
 
     // SQL query to insert data into the database
-    $insert_stock = "INSERT INTO stock_investment (stock_id, stock_title, stock_amount, stock_interest, stock_duration, stock_status) VALUES (:stock_id, :stock_title, :stock_amount, :stock_interest, :stock_duration, :stock_status)";
+    $insert_stock = "INSERT INTO stock_investment (stock_id, stock_title, stock_amount_min,stock_amount_max, stock_interest, stock_duration, stock_status) VALUES (:stock_id, :stock_title, :stock_amount_min,stock_amount_max, :stock_interest, :stock_duration, :stock_status)";
     $stock_db = $conn->prepare($insert_stock);
     $result = $stock_db->execute([
         'stock_id' => $stock_id,
         'stock_title' => $stock_title,
-        'stock_amount' => $stock_amount,
+        'stock_amount_min' => $stock_amount_min,
+        'stock_amount_max' => $stock_amount_max,
         'stock_interest' => $stock_interest,
         'stock_duration' => $stock_duration,
         'stock_status' => $stock_status
@@ -63,8 +65,16 @@ if (isset($_POST['create_stock'])) {
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group mb-4">
-                                                <label for="">Stock Amount: <span class="text-white bg-info p-1 rounded shadow-sm">USD</span>  </label>
-                                                <input type="number"  name="stock_amount" class="form-control" id="" placeholder="Stock Amount:" required>
+                                                <div class="row">
+                                                    <div class="col-sm-6">
+                                                        <label for="">Stock Amount Min: <span class="text-white bg-info p-1 rounded shadow-sm">USD</span>  </label>
+                                                        <input type="number"  name="stock_amount_min" class="form-control" id="" placeholder="Minimum Stock Amount:" required>
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <label for="">Stock Amount Max: <span class="text-white bg-info p-1 rounded shadow-sm">USD</span>  </label>
+                                                        <input type="number"  name="stock_amount_max" class="form-control" id="" placeholder="Maximum Stock Amount:" required>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
