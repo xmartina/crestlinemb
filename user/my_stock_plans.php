9b003c5996bbf845d87998e95d44963586f26e4b
unknown <martinsprince126@gmail.com>
2023-09-18 12:26:37 -0400

    updated

--- user/my_stock_plans.php
+++ user/my_stock_plans.php
@@ -37,7 +37,7 @@ $investment_data['investment_plan_name'];
                 <?php } ?>
                 <div class="bg-white py-2 px-3 rounded-2 mb-1">
                     <div class="row">
-                        <div class="col-sm-1">1</div>
+                        <div class="col-sm-1 d-none d-lg-block d-xl-block ">1</div>
                         <div class="col-sm-2 font-weight-bold">Plan Name</div>
                     </div>
                 </div>
