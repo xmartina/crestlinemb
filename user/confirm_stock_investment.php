de5c490b09759d58bdcbf8f24bb40e9a82d293bd
unknown <martinsprince126@gmail.com>
2023-09-17 21:18:19 -0400

    updated

--- user/confirm_stock_investment.php
+++ user/confirm_stock_investment.php
@@ -3,12 +3,16 @@ $pageName = "Confirm Stock Investments";
 include_once("layouts/header.php");
 $user_id = $row['id'];
 require_once("userPinfunction.php");
+$investment_id = $_GET['id'];
 
-$sql = "SELECT * FROM stock_investment WHERE id=:user_id";
+$sql = "SELECT * FROM stock_investment WHERE stock_id=:investment_id";
 $stmt = $conn->prepare($sql);
-$stmt->execute();
-$sn = 1;
+$stmt->execute([
+    'investment_id'=> $investment_id
+]);
 
+$invest_data = $stmt->fetch(PDO::FETCH_ASSOC);
+$invest_id = $invest_data['stock_id'];
 $get_user_data = "SELECT * FROM users WHERE id=:user_id";
 $user_data_hold = $conn->prepare($get_user_data);
 $user_data_hold->execute([
@@ -20,4 +24,4 @@ $user_data = $user_data_hold->fetch(PDO::FETCH_ASSOC);
 <div id="content" class="main-content">
     <div class="layout-px-spacing">
         <div class="row layout-top-spacing">
-<?=$user_data['id']; ?>
+<?=$invest_id; ?>
