81ce06f20dd63b92d1256891e002e9454e494522
unknown <martinsprince126@gmail.com>
2023-09-18 12:34:32 -0400

    updated

--- user/my_stock_plans.php
+++ user/my_stock_plans.php
@@ -29,12 +29,12 @@ $investment_data['investment_plan_name'];
         <div class="layout-px-spacing">
             <div class="layout-top-spacing">
                 <div class="row">
-                    <div class="col-lg-1 d-none d-lg-block d-xl-block ">S/N</div>
-                    <div class="col-lg-3 font-weight-bold">Plan Name</div>
-                    <div class="col-lg-2">Plans Duration</div>
-                    <div class="col-lg-2">Plan Returns</div>
-                    <div class="col-lg-1">Status</div>
-                    <div class="col-lg-3">Plan Reference No</div>
+                    <div class="col-lg-1 d-none d-lg-block d-xl-block  d-sm-none d-md-none d-lg-block d-xl-block">S/N</div>
+                    <div class="col-lg-3 font-weight-bold d-sm-none d-md-none d-lg-block d-xl-block">Plan Name</div>
+                    <div class="col-lg-2 d-sm-none d-md-none d-lg-block d-xl-block">Duration</div>
+                    <div class="col-lg-2 d-sm-none d-md-none d-lg-block d-xl-block">Returns</div>
+                    <div class="col-lg-1 d-sm-none d-md-none d-lg-block d-xl-block">Status</div>
+                    <div class="col-lg-3 d-sm-none d-md-none d-lg-block d-xl-block">Plan Reference No</div>
                 </div>
                 <?php while ($investment_data = $get_investment_hold->fetch(PDO::FETCH_ASSOC)) { ?>
                 <?php } ?>
@@ -42,7 +42,7 @@ $investment_data['investment_plan_name'];
                     <div class="row">
                         <div class="col-lg-1 d-none d-lg-block d-xl-block ">1</div>
                         <div class="col-lg-3 col-md-6 col-sm-12 font-weight-bold">Plan Name</div>
-                        <div class="col-lg-2 col-md-6 col-sm-12">Plans Duration</div>
+                        <div class="col-lg-2 col-md-6 col-sm-12"> <span class=""></span> Plans Duration</div>
                         <div class="col-lg-2 col-md-6 col-sm-12 font-weight-bold">Plan Returns</div>
                         <div class="col-lg-1 col-md-6 col-sm-12">Plan Status</div>
                         <div class="col-lg-3 col-md-6 col-sm-12 font-weight-bold">Plan Reference No</div>
