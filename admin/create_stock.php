b2ff16ed4c0a0bbcbac0219db7a6b1728a2bb484
unknown <martinsprince126@gmail.com>
2023-09-17 21:41:37 -0400

    updated

--- admin/create_stock.php
+++ admin/create_stock.php
@@ -16,7 +16,7 @@ if (isset($_POST['create_stock'])) {
     $stock_id = str_pad(rand(0, 9999999999), 10, '0', STR_PAD_LEFT);
 
     // SQL query to insert data into the database
-    $insert_stock = "INSERT INTO stock_investment (stock_id, stock_title, stock_amount_min,stock_amount_max, stock_interest, stock_duration, stock_status) VALUES (:stock_id, :stock_title, :stock_amount_min,stock_amount_max, :stock_interest, :stock_duration, :stock_status)";
+    $insert_stock = "INSERT INTO stock_investment (stock_id, stock_title, stock_amount_min,stock_amount_max, stock_interest, stock_duration, stock_status) VALUES (:stock_id, :stock_title, :stock_amount_min,:stock_amount_max, :stock_interest, :stock_duration, :stock_status)";
     $stock_db = $conn->prepare($insert_stock);
     $result = $stock_db->execute([
         'stock_id' => $stock_id,
