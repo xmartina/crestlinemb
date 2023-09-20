818af882fa70d8f3eb6b54f02aaee0c78ba2e227
unknown <martinsprince126@gmail.com>
2023-09-19 21:42:50 -0400

    updated

--- user/list_stock.php
+++ user/list_stock.php
@@ -59,12 +59,14 @@ $sn = 1;
                         <div class="row">
                             <div class="col-lg-4 plan_main_bg rounded shadow-sm">
                                 <div class="text-center">
-                                    <div class="h6 my-3 text-light">Gas Plan Name</div>
-                                    <div class="text-danger h3 mb-1">Plan Returns %</div>
-                                    <div class="text-light text-capitalize mb-3">After :plan_duration: Days</div>
-                                    <div class="h6 mb-2">Min : acct_currency : Plan_min</div>
-                                    <div class="h6 mb-2">Max : acct_currency : Plan_max</div>
-                                    <div class="btn bg-danger text-light my-3 px-3 py-2">Invest</div>
+                                    <div class="h6 my-3 text-light"><?= $row['stock_title'] ?></div>
+                                    <div class="text-danger h3 mb-1"><?= $row['stock_interest'] ?> %</div>
+                                    <div class="text-light text-capitalize mb-3">After <?= $row['stock_duration'] ?> Days</div>
+                                    <div class="h6 mb-2">Min : USD<?= $row['stock_amount_min'] ?></div>
+                                    <div class="h6 mb-2">Max : USD<?= $row['stock_amount_max'] ?></div>
+                                    <a href="<?php if ($row['stock_status'] == 1) { ?> ./confirm_stock_investment.php?id=<?php echo $row['stock_id']; ?> <?php }elseif($row['stock_status'] == 2){ ?> javascript:void(0); <?php } ?>" class="btn bg-danger text-light my-3">
+                                        <button class="btn bg-danger text-light px-3 py-2 <?php if ($row['stock_status'] == 2) { ?> disabled <?php } ?>"> Invest</button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
