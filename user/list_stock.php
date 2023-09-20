5f9dec9a6ba0a768f34eafc64bf6d8bf162e2291
unknown <martinsprince126@gmail.com>
2023-09-19 21:51:46 -0400

    updated

--- user/list_stock.php
+++ user/list_stock.php
@@ -25,12 +25,12 @@ $sn = 1;
                             <div class="col-lg-4">
 
                                 <div class="text-center plan_main_bg rounded shadow-sm">
-                                    <div class="h6 my-3 text-light"><?= $row['stock_title'] ?></div>
+                                    <div class="h5 my-3 pt-4 text-light"><?= $row['stock_title'] ?></div>
                                     <div class="text-danger h3 mb-1"><?= $row['stock_interest'] ?> %</div>
-                                    <div class="text-light text-capitalize mb-3">After <?= $row['stock_duration'] ?> Days</div>
+                                    <div class="text-light text-capitalize mb-4">After <?= $row['stock_duration'] ?> Days</div>
                                     <div class="h6 mb-2">Min : USD<?= $row['stock_amount_min'] ?></div>
                                     <div class="h6 mb-2">Max : USD<?= $row['stock_amount_max'] ?></div>
-                                    <a href="<?php if ($row['stock_status'] == 1) { ?> ./confirm_stock_investment.php?id=<?php echo $row['stock_id']; ?> <?php }elseif($row['stock_status'] == 2){ ?> javascript:void(0); <?php } ?>" class="btn bg-danger text-light my-3">
+                                    <a href="<?php if ($row['stock_status'] == 1) { ?> ./confirm_stock_investment.php?id=<?php echo $row['stock_id']; ?> <?php }elseif($row['stock_status'] == 2){ ?> javascript:void(0); <?php } ?>" class="my-3">
                                         <button class="btn bg-danger text-light px-3 py-2 <?php if ($row['stock_status'] == 2) { ?> disabled <?php } ?>"> Invest</button>
                                     </a>
                                 </div>
